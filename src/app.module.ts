cae9d890c8fd529605951cb155cf9e0a670cee5a
shch989 <tankpop11@gmail.com>
2023-04-27 23:22:57 +0900

    환경세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatsGateway } from './chats/chats.gateway';
+import { ChatsModule } from './chats/chats.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    ChatsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ChatsGateway],
 })
 export class AppModule {}
