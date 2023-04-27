161a19fee3ce7452c1506a065f839140a2ce3be7
shch989 <tankpop11@gmail.com>
2023-04-28 00:22:40 +0900

    socket on과 socket emit 실습

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -8,6 +8,11 @@ const formElement = getElementById('chat_form');
 
 function helloUser() {
   const username = prompt('What is your name?')
+  socket.emit("new_user", username)
+  console.log(username)
+  socket.on('hello_user', (data) => {
+    console.log(data)
+  })
 }
 
 function init() {
