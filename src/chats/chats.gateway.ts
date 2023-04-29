58e84d649dea71fbd6c913a85a228dde6f3a0d54
shch989 <tankpop11@gmail.com>
2023-04-29 23:24:23 +0900

    gateway 생명주기

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -2,12 +2,31 @@ import { Socket } from 'socket.io';
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
 
-@WebSocketGateway()
-export class ChatsGateway {
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+  constructor() {
+    this.logger.log('constructor');
+  }
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`disconnected : ${socket.id} ${socket.nsp.name}`);
+  }
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`connected : ${socket.id} ${socket.nsp.name}`);
+  }
+  afterInit() {
+    this.logger.log('init');
+  }
   @SubscribeMessage('new_user')
   handleNewUser(
     @MessageBody() username: string,
@@ -15,6 +34,6 @@ export class ChatsGateway {
   ) {
     console.log(socket.id);
     console.log(username);
-    socket.emit('hello_user', 'hello' + username)
+    socket.emit('hello_user', 'hello' + username);
   }
 }
